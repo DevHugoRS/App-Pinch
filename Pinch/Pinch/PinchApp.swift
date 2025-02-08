5ba34eb42abe94f69e31156c0087d39af4b0b2c3
DevHugoRS <eng.hugo.rs@gmail.com>
2025-02-08 13:00:37 +0100

    Inicio

---
+++ Pinch/Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Hugo Rodrigues on 08/02/25.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
