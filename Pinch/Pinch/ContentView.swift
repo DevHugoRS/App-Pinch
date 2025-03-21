1e43558c2fb6b0a52dbbb040a7447e744402abc6
DevHugoRS <eng.hugo.rs@gmail.com>
2025-03-21 10:45:37 +0100

    21/03

--- Pinch/Pinch/ContentView.swift
+++ Pinch/Pinch/ContentView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isAnimating: Bool = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+
+        NavigationView {
+            ZStack {
+//                MARK: - Page Image
+                Image("magazine-back-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+                
+            } //: ZStack
+            .navigationTitle("Asta & Yuno")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear(perform: {
+                withAnimation(.linear(duration: 2)) {
+                    isAnimating = true
+                }
+            })
+        } //: NavigationView
+        .navigationViewStyle(.stack)
+        
     }
 }
 
